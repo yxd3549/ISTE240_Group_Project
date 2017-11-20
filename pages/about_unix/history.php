76192f84a36ed16b78e10427aec7f5f88b02263d
ljd7969 <ljd7969@cs.rit.edu>
2017-11-19 23:13:54 -0500

    Fixed container style error

--- pages/about_unix/history.php
+++ pages/about_unix/history.php
@@ -65,13 +65,13 @@
     <div class="starter-template">
         <h1>Unix History</h1>
         <div class="timeline">
-            <div class="container left">
+            <div class="timecontainer left">
                 <div class="content">
                     <h2>2017</h2>
                     <p>Lorem ipsum..</p>
                 </div>
             </div>
-            <div class="container right">
+            <div class="timecontainer right">
                 <div class="content">
                     <h2>2016</h2>
                     <p>Lorem ipsum..</p>
