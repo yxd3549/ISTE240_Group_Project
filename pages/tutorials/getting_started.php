9dd5420c734053852b1fdb7e23038edde66e49d5
al8963 <al8963@g.rit.edu>
2017-11-20 15:30:15 -0500

    getting started tutorial

--- pages/tutorials/getting_started.php
+++ pages/tutorials/getting_started.php
@@ -144,11 +144,23 @@
                 We recommend you do not do this unless the server has stopped responding to you and you cannot close the window any other way.</li>
 
         </ul>
+        <br><br>
     <h4>On Another Unix-like System</h4>
    <ul><li>
            The process for logging in remotely to a Unix system from another Unix system is generally much more simple. SSH clients are normally built into bash and Mac Terminal, so when you open up a terminal on either a Unix/Mac OS,
                you can simply enter the command ‘ssh [hostname]’ with the hostname of your Unix system. You will be prompted to login just like you would be if you were at the machine itself.
        </li></ul>
+        <ol>
+       <li>First go to the upper right corner and click the search function and search for "terminal". </li>
+       <img src="../../assets/media/search_terminal.png" alt="search_terminal" height="425" width="673">
+       <li>The mac's terminal will be very similar to PuTTY and any other CLIs..</li>
+
+       <img src="../../assets/media/prompt1.png" alt="prompt1" height="160" width="586">
+       <li>Once you have the terminal opened, use the command 'ssh' +  the hostname you want to connect to.</li>
+       <img src="../../assets/media/mac_login.png" alt="mac_login" height="370" width="600">
+            <li>You will then be prompted to enter the username and password. Once you are successfully authenticated, you are good to go.</li>
+            <img src="../../assets/media/password.png" alt="password" height="367" width="585">
+   </ol>
     </div>
 </main><!-- /.container -->
 
