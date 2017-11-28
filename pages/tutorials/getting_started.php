daf108ba533df75c64e11527c2b34f8ff1b71303
abhigolchha <33706165+abhigolchha@users.noreply.github.com>
2017-11-28 11:29:41 -0500

    Footer CSS added.

--- pages/tutorials/getting_started.php
+++ pages/tutorials/getting_started.php
@@ -171,10 +171,24 @@
     </div>
 </main><!-- /.container -->
 
+<div id="footer">
+    blah blah blah
+    <p>
+        blah blah blah<br>
+        Blah blah blah.
+    </p>
+</div>
+<!-- LAYER TWO -->
+<div id="secondfooter">
+    Created for ISTE 240 Group project
+</div>
+
 
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
+
+
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
