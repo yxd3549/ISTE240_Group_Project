346b369bbe57104004878f8d70504c287b5d2829
Jake <jpf1980@rit.edu>
2017-11-27 23:15:20 -0500

    .

--- index.php
+++ index.php
@@ -82,15 +82,10 @@
         <h1>Welcome to ThunderFox Unix!</h1>
         <p class="lead">Unix is a powerful operating system, utilized in many computing fields for a variety of tasks. Using this site, you will be able to develop the skills and know-how to take advantage of Unix's benefits over its less advanced albeit less complicated co-operating systems.</p>
         <h3>Quick links</h3>
-        <span><a href="pages/about_landing.php"><img src="assets/media/history_thumb.png" alt="About Landing Page" /></a></span><br>
-        <span><a href="pages/tutorials_landing.php"><img src="assets/media/tutorial_thumb.png" alt="Tutorial Landing Page"/></a></span><br>
-        <span><a href="pages/quizzes_landing.php"><img src="assets/media/quiz_thumb.png" alt="Quiz Landing Page"/></a></span><br>
-        <span><a href="pages/references_landing.php"><img src="assets/media/ref_thumb.png" alt="References Landing Page"/></a></span>
-        <br>
-        <h3>Fields that utilize Unix</h3>
-        <span>Web Developer(Avg. salary: $87,661)</span><br>
-        <span>System Administrator(Avg. salary: $71,144)</span><br>
-        <span>Network Engineer(Avg. salary: $$78,480)</span><br>
+        <span><a href="pages/about_landing.php"><img src="assets/media/history_thumb.png" alt="About Landing Page" width="30%" height="auto"/></a></span>
+        <span><a href="pages/tutorials_landing.php"><img src="assets/media/tutorial_thumb.png" alt="Tutorial Landing Page" width="30%" height="auto"/></a></span>
+        <span><a href="pages/quizzes_landing.php"><img src="assets/media/quiz_thumb.png" alt="Quiz Landing Page" width="30%" height="auto"/></a></span>
+        <span><a href="pages/references_landing.php"><img src="assets/media/ref_thumb.png" alt="References Landing Page" width="30%" height="auto"/></a></span>
     </div>
 </main><!-- /.container -->
 
