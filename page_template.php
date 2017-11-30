fe8652f86176468a64ad40395df89aafef1fe803
BakariWilkins <bxw2270@rit.edu>
2017-11-29 20:31:14 -0500

    Added the footer php includes to all pages and to the page template.

--- page_template.php
+++ page_template.php
@@ -1,8 +1,8 @@
 <!--template not 100% finished-->
 
 <?php
-    $title = ""; /* Title to be show in tab*/
-    $rel_path_home = ""; /* relative path to the home directory. Ex: "../../ or "" */
+    $title = ""; /* Title to be shown in tab*/
+    $rel_path_home = "../../"; /* relative path to the home directory. Ex: "../../ or "" */
 
     /* Do Not Change*/ require_once $rel_path_home . "assets/php/page_start.php"; /* Do Not Change*/
 
@@ -20,14 +20,6 @@
 
 </main><!-- /.container -->
 
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-
-</body>
-</html>
+<?php
+    require_once PATH_PHP . "footer.php";
+?>
