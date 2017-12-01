c3566f2a57a9c127ba69555806cd05a725e0e710
BakariWilkins <bxw2270@rit.edu>
2017-12-01 10:33:12 -0500

    Add files via upload
    Updated template and README

--- page_template.php
+++ page_template.php
@@ -1,21 +1,19 @@
-<!--template not 100% finished-->
 
 <?php
-    $title = ""; /* Title to be shown in tab*/
+    $title = "****"; /* Title to be shown in tab*/
     $rel_path_home = "../../"; /* relative path to the home directory. Ex: "../../ or "" */
 
-    /* Do Not Change*/ require_once $rel_path_home . "assets/php/page_start.php"; /* Do Not Change*/
-
-//    require_once PATH_PHP . "utilities.php"; /* utilities has the quiz grading methods and maybe others in the future, but not all pages use this */
+    /* Don't change these pls*/
+    require_once $rel_path_home . "assets/php/page_start.php";
+    require_once PATH_PHP . "utilities.php"; /* utilities has the quiz grading methods and maybe others in the future, but not all pages use this */
     require_once PATH_PHP . "head.php";
 ?>
-
 
 <main role="main" class="container">
 
     <div class="starter-template">
-        <h1> </h1>
-        <p class="lead">C</p>
+        <h1>****</h1>
+        <p class="lead">****</p>
     </div>
 
 </main><!-- /.container -->
