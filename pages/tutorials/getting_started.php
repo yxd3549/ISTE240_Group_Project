0f906b2c3fb89846f4ad2a0512bc0058f99965e4
al8963 <al8963@g.rit.edu>
2017-12-06 14:09:50 -0500

    getting started for mac and windows

--- pages/tutorials/getting_started.php
+++ pages/tutorials/getting_started.php
@@ -8,7 +8,7 @@
 <main role="main" class="container">
 
     <div class="starter-template">
-        <h1>Getting Started Tutorial</h1>
+        <h1>Getting Started - Windows System</h1>
         <p class="lead">Natively, Unix OS’s are command line based and therefore require a means of using the command line interface to control the Unix environment.
             There are various ways to do this both on the Unix machine and remotely from another PC. Locally, all you have to do is plug your Unix machine into a monitor and hook up a keyboard to gain access to the Unix command shell.
             It is not uncommon for people to use remote sessions to log into and control their Unix machines though, so here is a walkthrough on some of the most common practices for doing this.</p>
@@ -92,22 +92,7 @@
 
         </ul>
         <br><br>
-    <h4>On Another Unix-like System</h4>
-   <ul><li>
-           The process for logging in remotely to a Unix system from another Unix system is generally much more simple. SSH clients are normally built into bash and Mac Terminal, so when you open up a terminal on either a Unix/Mac OS,
-               you can simply enter the command ‘ssh [hostname]’ with the hostname of your Unix system. You will be prompted to login just like you would be if you were at the machine itself.
-       </li></ul>
-        <ol>
-       <li>First go to the upper right corner and click the search function and search for "terminal". </li>
-       <img src="../../assets/media/search_terminal.png" alt="search_terminal" height="425" width="673">
-       <li>The mac's terminal will be very similar to PuTTY and any other CLIs..</li>
 
-       <img src="../../assets/media/prompt1.png" alt="prompt1" height="160" width="586">
-       <li>Once you have the terminal opened, use the command 'ssh' +  the hostname you want to connect to.</li>
-       <img src="../../assets/media/mac_login.png" alt="mac_login" height="370" width="600">
-            <li>You will then be prompted to enter the username and password. Once you are successfully authenticated, you are good to go.</li>
-            <img src="../../assets/media/password.png" alt="password" height="367" width="585">
-   </ol>
     </div>
 </main><!-- /.container -->
 
