7a6e378b072273efbe396530579791669dd320a7
Louis <ljd7969>
2017-12-06 19:31:58 -0500

    added what is unix

---
+++ pages/about_unix/what.php
@@ -0,0 +1,32 @@
+<?php
+    $title = "Linux";
+    $rel_path_home = "../../";
+    require_once $rel_path_home . "assets/php/page_start.php";
+    require_once PATH_PHP . "head.php";
+    require_once PATH_PHP . "utilities.php"
+?>
+<main role="main" class="container">
+
+    <div class="starter-template">
+        <h1>What Even is Unix?</h1>
+        <p class="lead">
+            Unix (officially UNIX) is a registered trademark of The Open Group that refers to a family of
+            computer operating systems and tools conforming to The Open Group Base Specification, Issue 7
+            (also known as POSIX.1-2008 or IEEE Std 1003.1 - 2008). To use the Unix trademark, an operating
+            system vendor must pay a licensing fee and annual trademark royalties to The Open Group. Officially
+            licensed Unix operating systems (and their vendors) include OS X (Apple), Solaris (Oracle), AIX (IBM),
+            IRIX (SGI), and HP-UX (Hewlett-Packard). </p><br/>
+
+            <aside>Note</aside><br/>
+
+            Operating systems that behave like Unix systems and provide similar utilities, but do not conform to Unix
+            specification or are not licensed by The Open Group, are commonly known as Unix-like systems. These include
+            a wide variety of Linux distributions (e.g., Red Hat Enterprise Linux, Ubuntu, and CentOS) and several descendants
+            of the Berkeley Software Distribution operating system (e.g., FreeBSD, OpenBSD, and NetBSD).
+        </p>
+    </div>
+</main><!-- /.container -->
+
+<?php
+    require_once PATH_PHP . "footer.php";
+?>
