a9a1e49d56e454d0b4d09cb6389992914d7dd499
ljd7969 <ljd7969@cs.rit.edu>
2017-12-07 00:28:48 -0500

    edited advantages page

--- pages/about_unix/components.php
+++ pages/about_unix/components.php
@@ -1,5 +1,5 @@
 <?php
-    $title = "Linux";
+    $title = "Components";
     $rel_path_home = "../../";
     require_once $rel_path_home . "assets/php/page_start.php";
     require_once PATH_PHP . "head.php";
