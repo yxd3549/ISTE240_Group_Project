e848de2a2e061b483ee66ffe114194303b819eae
Jake <jpf1980@rit.edu>
2017-12-07 07:01:33 -0500

    replaced links on homepage with images

--- pages/references_landing.php
+++ pages/references_landing.php
@@ -11,9 +11,8 @@
     $rel_path_home = "../../";
     require_once $rel_path_home . "assets/php/page_start.php";
     require_once PATH_PHP . "head.php";
-    require_once PATH_PHP . "utilities.php"
+    require_once PATH_PHP . "utilities.php";
 ?>
-
 <main role="main" class="container">
     <div class="starter-template">
         <h1 class="page-title">References</h1>
