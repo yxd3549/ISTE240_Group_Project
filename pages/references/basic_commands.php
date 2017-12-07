ac6fe86b96aaf0c80856015f839cbd6ce5b9ced8
abhigolchha <33706165+abhigolchha@users.noreply.github.com>
2017-12-06 22:59:23 -0500

    New page added

---
+++ pages/references/basic_commands.php
@@ -0,0 +1,21 @@
+<?php
+$title = "Basic Unix Commands";
+$rel_path_home = "../../";
+require_once $rel_path_home . "assets/php/page_start.php";
+require_once PATH_PHP . "head.php";
+require_once PATH_PHP . "utilities.php"
+?>
+
+    <main role="main" class="container">
+
+        <div class="starter-template">
+            <h1>Basic Unix Commands</h1>
+            <p class="lead">Want to use UNIX? Learn these commands.</p>
+        </div>
+        <ul><?php readQuestions("../../assets/text_files/basic_commands.txt");?></ul>
+
+    </main><!-- /.container -->
+
+<?php
+require_once PATH_PHP . "footer.php";
+?>
