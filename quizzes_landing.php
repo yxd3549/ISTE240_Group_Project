b0232f210260acaca38dd89bfb810693700cb516
Jake Floss <32464268+jfloss@users.noreply.github.com>
2017-12-07 00:06:52 -0500

    original "working" landing pages

---
+++ quizzes_landing.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * author: jake
+ * Date: 11/27/17
+ * Time: 2:32 PM
+ */
+?>
+<?php
+    $title = "Quizzes Landing";
+    $rel_path_home = "../";
+    require_once $rel_path_home . "assets/php/page_start.php";
+    require_once PATH_PHP . "head.php";
+    require_once PATH_PHP . "utilities.php";
+?>
+
+<main role="main" class="container">
+    <div class="starter-template">
+        <h1 class="page-title">Quizzes</h1>
+        <!-- content goes here -->
+        <span><a class="landing-links" href="<?=URL_PAGES?>quizzes/getting_started_quiz.php">Getting Started with Unix</a></span><br>
+		<span><a class="landing-links" href="<?=URL_PAGES?>quizzes/os_quiz.php">Operating Systems</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>quizzes/cli_quiz.php">Command Line Interface</a></span><br>
+		<span><a class="landing-links" href="<?=URL_PAGES?>quizzes/unix_components_quiz.php">Unix Components</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>quizzes/directory_structure_quiz.php">Directory Structure</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>quizzes/file_permissions_quiz.php">File Permissions</a></span><br>
+		<span><a class="landing-links" href="<?=URL_PAGES?>quizzes/outside_world_quiz.php">Connecting to the Outside World</a></span><br>
+		<span><a class="landing-links" href="<?=URL_PAGES?>quizzes/shell_scripting_quiz.php">Shell Scripting</a></span><br>
+		<span><a class="landing-links" href="<?=URL_PAGES?>quizzes/tarballs_quiz.php">Tarballs</a></span><br>
+		<span><a class="landing-links" href="<?=URL_PAGES?>quizzes/user_commands_quiz.php">User Commands</a></span><br>
+    </div>
+</main><!-- /.container -->
+
+
+<?php
+    require_once PATH_PHP . "footer.php";
+?>
---
+++ tutorials_landing.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * author: jake
+ * Date: 11/27/17
+ * Time: 2:32 PM
+ */
+?>
+<?php
+    $title = "Tutorials Landing";
+    $rel_path_home = "../";
+    require_once $rel_path_home . "assets/php/page_start.php";
+    require_once PATH_PHP . "head.php";
+    require_once PATH_PHP . "utilities.php";
+?>
+
+<main role="main" class="container">
+
+    <div class="starter-template">
+        <h1 class="page-title">Tutorials</h1>
+        <!-- content goes here -->
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/getting_started.php">Getting Started - Windows</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/getting_started_mac.php">Getting Started - Mac OS</a></span><br>
+		<span><a class="landing-links" href="<?=URL_PAGES?>tutorials/unix_basics.php">Basic Unix Commands</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/about_commands.php">User Commands</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/command_line_interface.php">Command Line Interface</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/what_is_os.php">What is an OS</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/directory_structure.php">Directory Structure</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/file_permission.php">File Permission</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/shell_scripting.php">Shell Scripting</a></span><br>      
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/unix_components.php">Unix Components</a></span><br>    
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/tar_balls.php">Tar Balls</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/debugging_scripts.php">Debugging Scripts</a></span><br>
+		<span><a class="landing-links" href="<?=URL_PAGES?>tutorials/connecting_public.php">Miscellaneous Commands</a></span><br>
+        <span><a class="landing-links" href="<?=URL_PAGES?>tutorials/directory_commands.php">Directory Commands</a></span><br>
+    </div>
+</main><!-- /.container -->
+
+<?php
+    require_once PATH_PHP . "footer.php";
+?>
